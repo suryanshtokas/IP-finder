418950b68b177b2e4b399a5b893dc32e60c0fa86
Suryansh Tokas <smartonedge@gmail.com>
2020-10-24 16:06:34 +0530

    Changes to the views

--- IPApp/views.py
+++ IPApp/views.py
@@ -10,9 +10,17 @@ def index(request):
     return render(request, 'index.html', context)
 
 def finder(request):
-    search = request.POST.get('search')
+    search = get_client_ip(request)
     data = ipapi.location(ip=search, output="json")
 
     context = {"data": data}
 
     return render(request, 'finder.html', context)
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
